f7a998476bf4e83ee1f93a37f97396866cee2aee
Tina Ho <tinaho94@gatech.edu>
2016-08-09 16:13:46 -0700

    fixed some UI

--- app/models/WikipedaDocsPage.java
+++ app/models/WikipedaDocsPage.java
@@ -13,19 +13,33 @@ public class WikipedaDocsPage {
     private int startPageNumber;
     private int lastPageNumber;
     private int totalResults;
+    private String displayingHTML;
     private String leftDisabled;
     private String rightDisabled;
     private String paginationHTML;
 
+    // from: 0-based index of the search results
     public WikipedaDocsPage(List<WikipediaDoc> docs, int from, int totalResults, String query) {
         this.docs = docs;
         this.from = from;
         this.totalResults = totalResults;
         this.maxPossiblePageNumber = totalResults / 10 + 1;
         this.currentPageNumber = from / 10 + 1;
-        this.startPageNumber = ((currentPageNumber - 5) < 1) ? 1 : currentPageNumber - 5;
-        this.lastPageNumber = ((currentPageNumber + 5) > maxPossiblePageNumber) ? maxPossiblePageNumber : currentPageNumber + 5;
+        this.startPageNumber = ((currentPageNumber - 4) <= 1) ? 1 : currentPageNumber - 4;
+        this.lastPageNumber = ((currentPageNumber + 4) > maxPossiblePageNumber) ? maxPossiblePageNumber : currentPageNumber + 4;
 
+        StringBuilder displayingHTMLStringBuilder = new StringBuilder();
+        displayingHTMLStringBuilder.append("<div class=\"displayHTML\">");
+        displayingHTMLStringBuilder.append("Displaying ");
+        displayingHTMLStringBuilder.append(from + 1);
+        displayingHTMLStringBuilder.append("-");
+        displayingHTMLStringBuilder.append(Math.min(from + 10, totalResults));
+        displayingHTMLStringBuilder.append(" of ");
+        displayingHTMLStringBuilder.append(totalResults);
+        displayingHTMLStringBuilder.append("</div>");
+        displayingHTML = displayingHTMLStringBuilder.toString();
+
+        // !!!!!!!!!
 
         if (currentPageNumber == 1) {
             leftDisabled = "disabled";
@@ -40,7 +54,9 @@ public class WikipedaDocsPage {
         }
 
         StringBuilder pagination = new StringBuilder();
-        pagination.append("<ul class=\"pagination\">").append('\n');
+        pagination.append("<center>");
+        pagination.append("<div id=\"pageNumber\">");
+        pagination.append("<ul class=\"paging\">").append('\n');
         pagination.append(" <li class=\"").append(leftDisabled)
                 .append("\"><a href=\"")
                 .append("/search?from=");
@@ -49,7 +65,7 @@ public class WikipedaDocsPage {
         pagination.append("&query=")
                 .append(query)
                 .append("\"><i class=\"material-icons\">chevron_left</i></a></li>");
-        for (int i = startPageNumber; i < lastPageNumber; i++) {
+        for (int i = startPageNumber; i <= lastPageNumber; i++) {
             pagination.append("<li class=\"");
             if (currentPageNumber == i) {
                 pagination.append("active");
@@ -72,13 +88,17 @@ public class WikipedaDocsPage {
         int afterFrom = (currentPageNumber) * 10;
         pagination.append(afterFrom);
         pagination.append("&query=").append(query)
-                .append("\"><i class=\"material-icons\">chevron_right</i></a></li>\n</ul>\n");
+                .append("\"><i class=\"material-icons\">chevron_right</i></a></li>\n</ul></div>\n");
+        pagination.append("</center>");
         this.paginationHTML = pagination.toString();
-
     }
 
     public List<WikipediaDoc> getDocs() {
         return docs;
+    }
+
+    public String getDisplayingHTML() {
+        return displayingHTML;
     }
 
     public String getPaginationHTML() {
